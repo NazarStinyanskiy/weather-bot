4319e7ef4b54c58583c1b8c0441ca0a5e595d5b8
NazarStinianskiy <nazarstinyanskiy@gmail.com>
2021-04-10 16:05:16 +0200

    add info command

--- src/main/java/ua/nazariy/weather/commands/info/StartCommand.java
+++ src/main/java/ua/nazariy/weather/commands/info/StartCommand.java
@@ -1,11 +1,10 @@
-package ua.nazariy.weather.commands;
+package ua.nazariy.weather.commands.info;
 
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
-public class StartCommand extends AbstractCommand{
+public class StartCommand extends InfoCommand{
 
     public StartCommand(String commandIdentifier, String description) {
         super(commandIdentifier, description);
@@ -13,8 +12,6 @@ public class StartCommand extends AbstractCommand{
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chat.getId()).setText(language.START_GREETINGS);
-        execute(absSender, sendMessage);
+        super.execute(absSender, chat.getId(), language.START_GREETINGS);
     }
 }
